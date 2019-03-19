dccab3feb2efad17fca41d786097ec769ff95caa
CodingCherry <cherry.alexeeva@gmail.com>
2019-03-19 21:58:46 +0700

    Initial Commit

---
+++ smack-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack-app
+//
+//  Created by Alexeeva Ekaterina on 3/19/19.
+//  Copyright © 2019 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
